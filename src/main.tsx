20f5a65ec7638d137c629df721357bde3e839908
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-01-30 17:51:14 +0530

    Basic Routes and Pages Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./pages/ErrorPage";
+import LandingPage from "./pages/LandingPage";
+import PatientRegistrationPage from "./pages/PatientRegistrationPage";
+import VisitFollowUpPage from "./pages/VisitFollowUpPage";
 
-createRoot(document.getElementById('root')!).render(
-  
-)
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
+			<Route path="/" element={<LandingPage />} />
+			<Route path="/registerPatient" element={<PatientRegistrationPage />} />
+			<Route path="/visit" element={<VisitFollowUpPage />} />
+			<Route path="/*" element={<ErrorPage />} />
+		</>
+	)
+);
+
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
---
+++ src/pages/PatientRegistrationPage.tsx
@@ -0,0 +1,8 @@
+
+const PatientRegistrationPage = () => {
+  return (
+    <div>PatientRegistrationPage</div>
+  )
+}
+
+export default PatientRegistrationPage
