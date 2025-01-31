a3cd20f06f7e82cc5e181feb7b086104760734ad
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-01-31 16:05:37 +0530

    Added Form Validation.

--- src/pages/VisitFollowUpComponent/FollowUpFormComponent.tsx
+++ src/pages/VisitFollowUpComponent/FollowUpFormComponent.tsx
@@ -1,26 +1,62 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import "./FollowUpFormComponent.css";
 
 interface FollowUpFormComponentProps {
 	index: number;
 }
 
-const FollowUpFormComponent = ({ index }: FollowUpFormComponentProps) => {
-	// const { register, handleSubmit } = useForm();
+const schema = z.object({
+	followUpDate: z
+		.string()
+		.min(1, { message: "This field is required" })
+		.refine(
+			(value) => {
+				const selectedDate = new Date(value);
+				const today = new Date();
+				return selectedDate <= today;
+			},
+			{ message: "Date must be in the past" }
+		),
+	followUpNotes: z.string().min(10, { message: "Please enter at least 10 characters" }),
+	checkbox1: z.boolean(),
+	checkbox2: z.boolean(),
+	checkbox3: z.boolean(),
+	checkbox4: z.boolean(),
+	missedDoses: z
+		.number({ invalid_type_error: "The field cannot be empty" })
+		.min(0, { message: "Missed Doses can't be negative" }),
+});
 
-	// const formSubmitHandler = () => {};
+type FormData = z.infer<typeof schema>;
+
+const FollowUpFormComponent = ({ index }: FollowUpFormComponentProps) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+		reset,
+	} = useForm<FormData>({
+		resolver: zodResolver(schema),
+		mode: "all",
+	});
+
+	const formSubmitHandler = (data: FormData) => {
+		console.log("Submitted Data: ", data);
+	};
 
 	return (
-		<form id="follow-up-form">
+		<form id="follow-up-form" onSubmit={handleSubmit(formSubmitHandler)}>
 			<h2 className="form-title">Follow Up {index}</h2>
 
 			<div className="form-section">
 				<div className="form-group">
 					<label htmlFor="follow-up-date">Follow Up Date</label>
-					<input
-						type="date"
-						id="follow-up-date"
-						// {...register("followUpDate", { required: "true" })}
-					/>
+					<input type="date" id="follow-up-date" {...register("followUpDate")} />
+					{errors.followUpDate && (
+						<p className="error-message">{errors.followUpDate.message}</p>
+					)}
 				</div>
 			</div>
 
@@ -31,28 +67,31 @@ const FollowUpFormComponent = ({ index }: FollowUpFormComponentProps) => {
 						id="followup-notes"
 						rows={4}
 						placeholder="Enter detailed follow-up notes here..."
-						// {...register("followUpNotes", { required: "true" })}
+						{...register("followUpNotes")}
 					/>
+					{errors.followUpNotes && (
+						<p className="error-message">{errors.followUpNotes.message}</p>
+					)}
 				</div>
 			</div>
 
 			<div className="form-row">
 				<div>
 					<div className="checkbox-group">
-						<input type="checkbox" id="is-alive-input" />
-						<label htmlFor="is-alive-input">Is Patient Alive?</label>
+						<input type="checkbox" id="checkbox1" {...register("checkbox1")} />
+						<label htmlFor="checkbox1">Checkbox 1</label>
 					</div>
 					<div className="checkbox-group">
-						<input type="checkbox" id="is-alive-input" />
-						<label htmlFor="is-alive-input">Checkbox 2</label>
+						<input type="checkbox" id="checkbox2" {...register("checkbox2")} />
+						<label htmlFor="checkbox2">Checkbox 2</label>
 					</div>
 					<div className="checkbox-group">
-						<input type="checkbox" id="is-alive-input" />
-						<label htmlFor="is-alive-input">Checkbox 3</label>
+						<input type="checkbox" id="checkbox3" {...register("checkbox3")} />
+						<label htmlFor="checkbox3">Checkbox 3</label>
 					</div>
 					<div className="checkbox-group">
-						<input type="checkbox" id="is-alive-input" />
-						<label htmlFor="is-alive-input">Checkbox 4</label>
+						<input type="checkbox" id="checkbox4" {...register("checkbox4")} />
+						<label htmlFor="checkbox4">Checkbox 4</label>
 					</div>
 				</div>
 
@@ -62,17 +101,19 @@ const FollowUpFormComponent = ({ index }: FollowUpFormComponentProps) => {
 						type="number"
 						id="dosage-missed-input"
 						placeholder="Enter number of doses missed"
-						min="0"
-						// {...register("missedDoses", { required: "true" })}
+						{...register("missedDoses", { valueAsNumber: true })}
 					/>
+					{errors.missedDoses && (
+						<p className="error-message">{errors.missedDoses.message}</p>
+					)}
 				</div>
 			</div>
 
 			<div className="form-actions">
-				<button type="button" className="btn cancel-btn">
+				<button type="button" className="btn cancel-btn" onClick={() => reset()}>
 					Cancel
 				</button>
-				<button type="submit" className="btn submit-btn">
+				<button type="submit" className="btn submit-btn" disabled={!isValid}>
 					Submit
 				</button>
 			</div>
