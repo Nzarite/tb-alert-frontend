a3bd6ebcd271b27f3035ae8a7f6216e93a073bef
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-01-31 18:24:22 +0530

    wip - Revamped the styling, added Sidebar

--- src/pages/VisitFollowUpComponent/VisitFollowUpPage.tsx
+++ src/pages/VisitFollowUpComponent/VisitFollowUpPage.tsx
@@ -1,30 +1,32 @@
+import { Grid2 } from "@mui/material";
 import { useState } from "react";
 import FollowUpFormComponent from "./FollowUpFormComponent";
+import FollowUpSidebar from "./FollowUpSidebar";
 import "./VisitFollowUpPage.css";
 
 const VisitFollowUpPage = () => {
-	const [noOfFollowUps, setNoOfFollowUps] = useState(8);
+	const [noOfFollowUps] = useState(3);
+	const [selectedIndex, setSelectedIndex] = useState(0);
+
+	const handleListItemClick = (index: number) => {
+		setSelectedIndex(index);
+	};
 
 	return (
-		<div className="visit-follow-up-page">
-			{Array.from({ length: noOfFollowUps }).map((_, index) => (
-				<FollowUpFormComponent key={index} index={index + 1} />
-			))}
-			<div style={{ width: "30%", display: "flex", justifyContent: "space-evenly" }}>
-				<button
-					className="btn remove-btn"
-					style={{ width: "40%" }}
-					onClick={() => setNoOfFollowUps(Math.max(8, noOfFollowUps - 1))}>
-					Remove
-				</button>
-				<button
-					className="btn submit-btn"
-					style={{ width: "40%" }}
-					onClick={() => setNoOfFollowUps(Math.max(8, noOfFollowUps + 1))}>
-					Add
-				</button>
-			</div>
-		</div>
+		<Grid2 container>
+			<Grid2 size={2}>
+				<FollowUpSidebar
+					noOfFollowUps={noOfFollowUps}
+					selectedIndex={selectedIndex}
+					setIndex={handleListItemClick}
+				/>
+			</Grid2>
+			<Grid2 size={10}>
+				<div style={{ maxHeight: "92vh", overflow: "scroll", overflowX: "hidden" }}>
+					<FollowUpFormComponent index={selectedIndex + 1} />
+				</div>
+			</Grid2>
+		</Grid2>
 	);
 };
 
