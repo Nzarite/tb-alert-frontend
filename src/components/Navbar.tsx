fc1d68adc045f8fa0e6a7889fedf2b8cf00ccf18
Shashank Balakrishnan <shashank.balakrishnan@beehyv.com>
2025-02-05 12:09:02 +0530

    Navbar modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,26 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { MdLogout } from "react-icons/md";
+import { RiAccountBoxFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-	return (
-		<AppBar
-			position="sticky"
-			elevation={0}
-			sx={{
-				backgroundColor: "primary.main",
-				borderRadius: "0",
-			}}>
-			<Toolbar
-				variant="dense"
-				sx={{
-					minHeight: 50,
-					display: "flex",
-					justifyContent: "space-between",
-					alignItems: "center",
-				}}>
-				<Typography
-					variant="h6"
-					component="div"
-					sx={{
-						fontWeight: 600,
-						letterSpacing: "0.5px",
-						fontSize: 20,
-					}}>
-					TB Alert
-				</Typography>
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <div id="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-text">
+          TB Alert
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <Link to="#" className="navbar-text">
+          <RiAccountBoxFill title="Profile" size={25} />
+        </Link>
+        <Link to="#" className="navbar-text">
+          <MdLogout title="Log Out" size={25} />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
