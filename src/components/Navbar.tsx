d1e9b8dc748726378af254889270df73e6b86592
Shashank Balakrishnan <shashank.balakrishnan@beehyv.com>
2025-02-05 12:09:02 +0530

    Navbar modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,26 @@
+import { MdLogout } from "react-icons/md";
+import { RiAccountBoxFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-	return (
-		<div id="navbar">
-			<div>TB Alert</div>
-		</div>
-	);
+  return (
+    <div id="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-text">
+          TB Alert
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <Link to="#" className="navbar-text">
+          <RiAccountBoxFill title="Profile" size={25} />
+        </Link>
+        <Link to="#" className="navbar-text">
+          <MdLogout title="Log Out" size={25} />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
