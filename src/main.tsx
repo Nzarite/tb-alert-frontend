243559a76b4e68cc718deebc9bd1462e65c22f6b
Shashank Balakrishnan <shashank.balakrishnan@beehyv.com>
2025-02-05 12:14:32 +0530

    Landing page completed with routes to other pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,32 @@
 import { createRoot } from "react-dom/client";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./pages/ErrorPage";
 import LandingPage from "./pages/LandingPage";
 import PageLayout from "./pages/PageLayout";
 import PatientRegistrationPage from "./pages/PatientRegistrationPage";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
 import VisitFollowUpPage from "./pages/VisitFollowUpComponent/VisitFollowUpPage";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route element={<PageLayout />}>
-			<Route path="/" element={<LandingPage />} />
-			<Route path="/registerPatient" element={<PatientRegistrationPage />} />
-			<Route path="/visit" element={<VisitFollowUpPage />} />
-			<Route path="*" element={<ErrorPage />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route element={<PageLayout />}>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/registerPatient" element={<PatientRegistrationPage />} />
+      <Route path="/visit" element={<VisitFollowUpPage />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="settings" element={<Settings />} />
+    </Route>
+  )
 );
 
-createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
