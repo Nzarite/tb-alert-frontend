38e1caf28019f1bf1c6615b86757dc789a8db031
Shashank Balakrishnan <shashank.balakrishnan@beehyv.com>
2025-02-05 12:14:32 +0530

    Landing page completed with routes to other pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,61 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { createRoot } from "react-dom/client";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./pages/Error/ErrorPage";
 import LandingPage from "./pages/Homepage/LandingPage";
 import PageLayout from "./pages/PageLayout";
 import PatientRegistrationPage from "./pages/Registration/Patient/PatientRegistrationPage";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
 import StateCoordinatorRegistrationPage from "./pages/Registration/StateCoordinator/StateCoordinatorRegistrationPage";
 import TeleCommunicatorRegistration from "./pages/Registration/Telecommunicator/TeleCommunicatorRegistration";
 import VisitFollowUpPage from "./pages/FollowUp/VisitFollowUpPage";
 
-const theme = createTheme({
-	palette: {
-		background: {
-			default: "#fafafa",
-		},
-		primary: {
-			main: "#0B455C",
-		},
-	},
-	components: {
-		MuiPaper: {
-			styleOverrides: {
-				root: {
-					borderRadius: 12,
-				},
-			},
-		},
-	},
-	typography: {
-		fontSize: 12,
-	},
-});
 
+const theme = createTheme({
+    palette: {
+        background: {
+            default: "#fafafa",
+        },
+        primary: {
+            main: "#0B455C",
+        },
+    },
+    components: {
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 12,
+                },
+            },
+        },
+    },
+    typography: {
+        fontSize: 12,
+    },
+});
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route element={<PageLayout />}>
-			<Route path="/" element={<LandingPage />} />
-			<Route path="/registerPatient" element={<PatientRegistrationPage />} />
-			<Route path="/visit" element={<VisitFollowUpPage />} />
-			<Route
+  createRoutesFromElements(
+    <Route element={<PageLayout />}>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/registerPatient" element={<PatientRegistrationPage />} />
+      <Route path="/visit" element={<VisitFollowUpPage />} />
+      <Route
 				path="/register/state-coordinator"
 				element={<StateCoordinatorRegistrationPage />}
 			/>
 			<Route path="/register/telecommunicator" element={<TeleCommunicatorRegistration />} />
-			<Route path="*" element={<ErrorPage />} />
-		</Route>
-	)
+			<Route path="/reports" element={<Reports />} />
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="settings" element={<Settings />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")!).render(
