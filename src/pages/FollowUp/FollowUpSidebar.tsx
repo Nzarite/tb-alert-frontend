f9a52524249c060d94d8cfe20b1bbcb4c7e3b710
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-02-05 18:40:48 +0530

    (Work in Progress) Added API requests

--- src/pages/FollowUp/FollowUpSidebar.tsx
+++ src/pages/FollowUp/FollowUpSidebar.tsx
@@ -1,14 +1,14 @@
-import { Chip } from "@mui/material";
+import { Chip, Divider, Typography } from "@mui/material";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import { Pencil } from "react-bootstrap-icons";
-import { FollowUpsDataInterface } from "../../components/VisitDataTypes";
+import { VisitDataInterface } from "../../components/VisitDataTypes";
 
 interface FollowUpSidebarProps {
 	selectedIndex: number;
 	setIndex: (index: number) => void;
-	data: FollowUpsDataInterface[];
+	data: VisitDataInterface;
 }
 
 export default function FollowUpSidebar({ selectedIndex, setIndex, data }: FollowUpSidebarProps) {
@@ -37,7 +37,11 @@ export default function FollowUpSidebar({ selectedIndex, setIndex, data }: Follo
 
 	return (
 		<List component="nav" aria-label="follow-up options">
-			{data.map((item, index) => (
+			<Typography variant="h6" sx={{ marginBottom: 2 }}>
+				Patient: {data.patient.firstName} {data.patient.lastName}
+			</Typography>
+			<Divider sx={{ marginBottom: 2 }} />
+			{data.followUpDetails.map((item, index) => (
 				<ListItemButton
 					key={index}
 					selected={selectedIndex === index}
@@ -50,8 +54,8 @@ export default function FollowUpSidebar({ selectedIndex, setIndex, data }: Follo
 					disableTouchRipple>
 					<ListItemText primary={`Follow Up ${index + 1}`} secondary={item.date} />
 					<Chip
-						label={getStatusName(item.date, item.filled)}
-						color={getStatusColor(item.date, item.filled)}
+						label={getStatusName(item.date, item.followUpStatus)}
+						color={getStatusColor(item.date, item.followUpStatus)}
 						variant="outlined"
 						size="small"
 					/>
--- src/pages/FollowUp/VisitFollowUpPage.tsx
+++ src/pages/FollowUp/VisitFollowUpPage.tsx
@@ -1,38 +1,46 @@
-import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import { Box, Container, Grid, Paper } from "@mui/material";
 import { useEffect, useState } from "react";
-import VisitData from "../../components/Json/visit.json";
+import axiosInstance from "../../components/axiosInstance";
+import SearchBox from "../../components/SearchBox";
 import { VisitDataInterface } from "../../components/VisitDataTypes";
 import FollowUpFormComponent from "./FollowUpFormComponent";
 import FollowUpSidebar from "./FollowUpSidebar";
-import SearchBox from "../../components/SearchBox";
 
 const VisitFollowUpPage = () => {
-	const [data, setData] = useState<VisitDataInterface | null>(VisitData);
+	const [data, setData] = useState<VisitDataInterface | null>(null);
 	const [search, setSearch] = useState<string | null>(null);
-	const [selectedIndex, setSelectedIndex] = useState(() => {
-		const today = Date.now();
-
-		let index = 0;
-		if (data?.followUps.length) {
-			let fud = Date.parse(data.followUps[0].date);
-
-			for (let i = 0; i < data?.followUps.length; i++) {
-				const followUpDate = Date.parse(data.followUps[i].date);
-				if (followUpDate < today && followUpDate > fud) {
-					fud = followUpDate;
-					index = i;
-				}
-			}
-		}
-		return index;
-	});
+	const [selectedIndex, setSelectedIndex] = useState(0);
 
 	const handleListItemClick = (index: number) => {
-		if (data && index >= 0 && index < data?.followUps.length) setSelectedIndex(index);
+		if (data && index >= 0 && index < data?.followUpDetails.length) setSelectedIndex(index);
 	};
 
-	const getPatientData = (search: string) => {
-		setData(VisitData);
+	const getPatientData = async (search: string) => {
+		try {
+			const res = await axiosInstance.get(`/followup/${search}`);
+			setData(res.data);
+			console.log(data);
+
+			// Showing the current(to be filled or last filled) followup initially
+			setSelectedIndex(() => {
+				const today = Date.now();
+
+				if (!data?.followUpDetails?.length) return 0;
+				let index = 0;
+				let fud = Date.parse(data.followUpDetails[0].date);
+
+				for (let i = 0; i < data?.followUpDetails.length; i++) {
+					const followUpDate = Date.parse(data.followUpDetails[i].date);
+					if (followUpDate < today && followUpDate > fud) {
+						fud = followUpDate;
+						index = i;
+					}
+				}
+				return index;
+			});
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	useEffect(() => {
@@ -48,9 +56,6 @@ const VisitFollowUpPage = () => {
 			<Container maxWidth="xl">
 				<Grid container spacing={{ xs: 2, md: 3 }}>
 					<Grid item xs={12} md={3}>
-						{/* Search Bar */}
-						<SearchBox changeSearch={(input) => setSearch(input)} />
-
 						{/* Sidebar */}
 						<Paper
 							variant="outlined"
@@ -58,13 +63,14 @@ const VisitFollowUpPage = () => {
 								padding: 2,
 								position: "sticky",
 								top: 16,
-								marginTop: 1,
+								height: "84vh",
+								overflow: "auto",
 							}}>
-							{data && data.followUps.length > 0 && (
+							{data && data.followUpDetails.length > 0 && (
 								<FollowUpSidebar
 									selectedIndex={selectedIndex}
 									setIndex={handleListItemClick}
-									data={data.followUps}
+									data={data}
 								/>
 							)}
 						</Paper>
@@ -72,15 +78,12 @@ const VisitFollowUpPage = () => {
 
 					{/* Main Content */}
 					<Grid item xs={12} md={9}>
-						{data && (
-							<Typography variant="h5" sx={{ marginTop: 1, marginBottom: 1 }}>
-								Patient Name: {data.patient.firstname} {data.patient.lastname}
-							</Typography>
-						)}
+						{/* Search Bar */}
+						<SearchBox changeSearch={(input) => setSearch(input.value)} />
 
 						<FollowUpFormComponent
 							index={selectedIndex + 1}
-							data={data && data.followUps[selectedIndex]}
+							data={data && data.followUpDetails[selectedIndex]}
 						/>
 					</Grid>
 				</Grid>
