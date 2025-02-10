100bfb55071634d47c3d5553dedb24145c92454d
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-02-10 10:37:50 +0530

    feat: Added localStorage support with redux

--- src/redux/langSlice.ts
+++ src/redux/langSlice.ts
@@ -1,15 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-	language: "en",
+interface LanguageState {
+	language: string;
+}
+
+const getInitialLanguage = (): string => {
+	const storedLang = localStorage.getItem("lang");
+	if (storedLang) return storedLang;
+
+	const defaultLang = "en";
+	localStorage.setItem("lang", defaultLang);
+	return defaultLang;
+};
+
+const initialState: LanguageState = {
+	language: getInitialLanguage(),
 };
 
 export const langSlice = createSlice({
 	name: "language",
 	initialState,
 	reducers: {
-		updateLanguage: (state, action) => {
-			state.language = action.payload;
+		updateLanguage: (state, action: PayloadAction<string>) => {
+			if (state.language !== action.payload) {
+				state.language = action.payload;
+				localStorage.setItem("lang", state.language);
+			}
 		},
 	},
 });
