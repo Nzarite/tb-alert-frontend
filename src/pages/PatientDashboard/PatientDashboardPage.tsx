1e99831c27c7bac2a951f8f9db06d128633b0823
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-02-19 12:58:35 +0530

    Changed position of medicine and contact screening

--- src/pages/PatientDashboard/PatientDashboardPage.tsx
+++ src/pages/PatientDashboard/PatientDashboardPage.tsx
@@ -72,19 +72,19 @@ const PatientDashboardPage = () => {
             prop: "tbdetails",
           },
           {
-            title: "Medicines",
-            component: <PatientMedicineDetails patientId={patientId} />,
-            icon: <MdVaccines style={{ fontSize: "20px" }} />,
-            size: 6,
-            editURL: "",
-          },
-          {
             title: "Contact Screening",
             component: <PatientContactScreeningDetails patientId={patientId} />,
             icon: <MdAssessment style={{ fontSize: "22px" }} />,
             size: 6,
             editURL: null,
             prop: "contactscreening",
+          },
+          {
+            title: "Medicines",
+            component: <PatientMedicineDetails patientId={patientId} />,
+            icon: <MdVaccines style={{ fontSize: "20px" }} />,
+            size: 6,
+            editURL: "",
           },
           {
             title: "Follow Up",
