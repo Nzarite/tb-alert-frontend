880de68572061ad87e2f34500278e40f851cae64
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-02-19 13:49:09 +0530

    imported and displayed patientConditionLabels

--- src/pages/PatientDashboard/PatientFollowUpDetails.tsx
+++ src/pages/PatientDashboard/PatientFollowUpDetails.tsx
@@ -1,96 +1,103 @@
 import {
-	Chip,
-	Divider,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Typography,
+  Chip,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../components/axiosInstance";
 import { FollowUpsDataInterface } from "../../components/datatypes/DataTypes";
+import { patientConditionLabels } from "../FollowUp/FollowUpMain";
 import { getStatusColor, getStatusName } from "../FollowUp/FollowUpSidebar";
 
-const PatientFollowUpDetails = (patientId:any) => {
-	const [patientData, setPatientData] = useState(null);
+const PatientFollowUpDetails = (patientId: any) => {
+  const [patientData, setPatientData] = useState(null);
 
-	useEffect(() => {
-		const getData = async () => {
-			const res = await axiosInstance.get(`followup/${patientId.patientId}`);
-			setPatientData(res.data.followUpDetails);
-		};
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axiosInstance.get(`followup/${patientId.patientId}`);
+      setPatientData(res.data.followUpDetails);
+    };
 
-		getData();
-	}, []);
+    getData();
+  }, []);
 
-	return (
-		<>
-			<Divider sx={{ mb: 4 }} />
+  return (
+    <>
+      <Divider sx={{ mb: 4 }} />
 
-			<TableContainer>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<TableCell sx={{ backgroundColor: "#ebebeb" }}>
-								<Typography sx={{ fontWeight: "bold" }}>Follow Up</Typography>
-							</TableCell>
-							<TableCell sx={{ backgroundColor: "#ebebeb" }}>
-								<Typography sx={{ fontWeight: "bold" }}>Date</Typography>
-							</TableCell>
-							<TableCell sx={{ backgroundColor: "#ebebeb" }}>
-								<Typography sx={{ fontWeight: "bold" }}>Status</Typography>
-							</TableCell>
-							<TableCell sx={{ backgroundColor: "#ebebeb" }}>
-								<Typography sx={{ fontWeight: "bold" }}>Review</Typography>
-							</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{patientData && patientData.length > 0 ? (
-							patientData.map((followup: FollowUpsDataInterface, index: number) => (
-								<TableRow key={index}>
-									<TableCell>
-										<Typography>Follow up {index + 1}</Typography>
-									</TableCell>
-									<TableCell>
-										<Typography>{followup.date}</Typography>
-									</TableCell>
-									<TableCell>
-										<Chip
-											label={getStatusName(
-												followup.date,
-												followup.followUpStatus
-											)}
-											color={getStatusColor(
-												followup.date,
-												followup.followUpStatus
-											)}
-											variant="outlined"
-											size="small"
-										/>
-									</TableCell>
-									<TableCell>
-										<Typography>{followup.patientCondition}</Typography>
-									</TableCell>
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell colSpan={4} align="center">
-									<Typography variant="body2" color="textSecondary">
-										No Follow Ups Found
-									</Typography>
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</>
-	);
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ backgroundColor: "#ebebeb" }}>
+                <Typography sx={{ fontWeight: "bold" }}>Follow Up</Typography>
+              </TableCell>
+              <TableCell sx={{ backgroundColor: "#ebebeb" }}>
+                <Typography sx={{ fontWeight: "bold" }}>Date</Typography>
+              </TableCell>
+              <TableCell sx={{ backgroundColor: "#ebebeb" }}>
+                <Typography sx={{ fontWeight: "bold" }}>Status</Typography>
+              </TableCell>
+              <TableCell sx={{ backgroundColor: "#ebebeb" }}>
+                <Typography sx={{ fontWeight: "bold" }}>
+                  Patient Condition
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {patientData && patientData.length > 0 ? (
+              patientData.map(
+                (followup: FollowUpsDataInterface, index: number) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <Typography>Follow up {index + 1}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{followup.date}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Chip
+                        label={getStatusName(
+                          followup.date,
+                          followup.followUpStatus
+                        )}
+                        color={getStatusColor(
+                          followup.date,
+                          followup.followUpStatus
+                        )}
+                        variant="outlined"
+                        size="small"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Typography>
+                        {patientConditionLabels[followup.patientCondition]}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                )
+              )
+            ) : (
+              <TableRow>
+                <TableCell colSpan={4} align="center">
+                  <Typography variant="body2" color="textSecondary">
+                    No Follow Ups Found
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default PatientFollowUpDetails;
