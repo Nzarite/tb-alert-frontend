6b97fcdcdc10f90ececa7285480f169a085330b1
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-02-24 18:50:40 +0530

    fixed aliveOrDead handling (sends "alive"/"dead") based on switch

--- src/pages/FollowUp/FollowUpMain.tsx
+++ src/pages/FollowUp/FollowUpMain.tsx
@@ -46,7 +46,7 @@ export const patientConditionLabels: { [key: number]: string } = {
 // Updated schema: currentStatus as boolean, patientCondition as number, etc.
 const schema = z.object({
   remarks: z.string().min(1, "Description can't be null"),
-  currentStatus: z.boolean(),
+  aliveOrDead: z.boolean(),
   missedMedications: z.array(
     z.object({
       medicationId: z.number(),
@@ -99,7 +99,7 @@ const FollowUpFormComponent = ({ index, data, getPatientData }: Props) => {
   useEffect(() => {
     if (data) {
       reset({
-        currentStatus: data?.patient.currentStatus === "alive",
+        aliveOrDead: data?.patient.currentStatus === "alive",
         patientCondition: data?.followUpDetails[index]?.patientCondition ?? 4,
         remarks: data?.followUpDetails[index]?.remarks ?? "",
         cured: data?.patient.cured ?? false,
@@ -125,6 +125,7 @@ const FollowUpFormComponent = ({ index, data, getPatientData }: Props) => {
       const submitData = {
         ...formData,
         date: data?.followUpDetails[index].date,
+        aliveOrDead: formData.aliveOrDead ? "alive" : "dead",
       };
       await axiosInstance.put(
         `/followup/${data?.patient.patientId}`,
@@ -217,7 +218,7 @@ const FollowUpFormComponent = ({ index, data, getPatientData }: Props) => {
           <Box component="form" onSubmit={handleSubmit(formSubmitHandler)}>
             <Stack spacing={4}>
               <Controller
-                name="currentStatus"
+                name="aliveOrDead"
                 control={control}
                 disabled={!isEditable}
                 render={({ field }) => (
