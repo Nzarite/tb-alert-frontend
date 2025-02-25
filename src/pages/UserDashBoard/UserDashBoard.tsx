dad636a99300af513a9cb5a62e17ec723d73dc81
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2025-02-25 18:53:50 +0530

    solved conflicts

--- src/pages/UserDashBoard/UserDashBoard.tsx
+++ src/pages/UserDashBoard/UserDashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import UserPersonalDetails from "./UserPersonalDetails";
 import { Paper, Box } from "@mui/material";
