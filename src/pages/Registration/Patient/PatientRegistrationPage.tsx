69db8253eb4f21e9c3513a2812943ff6a1d93f19
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2025-02-26 11:54:43 +0530

    changes to fix tbdetails error

--- src/pages/Registration/Patient/PatientRegistrationPage.tsx
+++ src/pages/Registration/Patient/PatientRegistrationPage.tsx
@@ -1,31 +1,25 @@
 import {
+  Alert,
   Container,
   Grid,
   Paper,
+  Snackbar,
   Step,
   StepLabel,
   Stepper,
-  Typography,
+  Typography
 } from "@mui/material";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useAuth } from "react-oidc-context";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
-import ContactScreeningDetailsForm, {
-  ContactScreeningData,
-} from "../../../components/ContactScreeningDetailsForm/ContactScreeningDetailsForm";
-import NikshayDetailsForm, {
-  NikshayDetailsData,
-} from "../../../components/NikshayDetailsForm/NikshayDetailsForm";
-import PatientDetailsForm, {
-  PatientDetailsData,
-} from "../../../components/PatientDetailsForm/PatientDetailsForm";
-import TbDetailsForm, {
-  TbDetailsData,
-} from "../../../components/TbDetailsForm/TbDetailsForm";
+import ContactScreeningDetailsForm, { ContactScreeningData } from "../../../components/ContactScreeningDetailsForm/ContactScreeningDetailsForm";
+import NikshayDetailsForm, { NikshayDetailsData } from "../../../components/NikshayDetailsForm/NikshayDetailsForm";
+import PatientDetailsForm, { PatientDetailsData } from "../../../components/PatientDetailsForm/PatientDetailsForm";
+import TbDetailsForm, { TbDetailsData } from "../../../components/TbDetailsForm/TbDetailsForm";
 import axiosInstance from "../../../components/axiosInstance";
-
+ 
 const PatientRegistrationPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -33,47 +27,53 @@ const PatientRegistrationPage = () => {
     location.state?.initialStep || 0
   );
   const [patientId, setPatientId] = useState<string | null>(null);
-
+  const [patientName, setPatientName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+ 
   const auth = useAuth();
   const userEmail =
-    useSelector((state) => state.user.profile.email) ||
-    auth.user?.profile.email;
-
+    useSelector((state) => state.user?.profile?.email) ||
+    auth.user?.profile?.email;
+ 
   const steps = [
     "Patient Details",
     "TB Details",
     "Nikshay Details",
     "Contact Screening Details",
   ];
-
+ 
   const [formData, setFormData] = useState({
     patientDetails: {} as PatientDetailsData,
     tbDetails: {} as TbDetailsData,
     nikshayDetails: {} as NikshayDetailsData,
     contactScreeningDetails: {} as ContactScreeningData,
   });
-
-  // const [language, setLanguage] = useState("en");
-
+ 
   const language = useSelector((state: any) => state.language.language);
-  console.log(language);
-
+ 
   const { handleSubmit } = useForm();
-
+ 
   const onSubmit = (data: any) => {
     console.log("Final Submitted Data:", data);
   };
-
+ 
   const handleSave = async (stepData: any) => {
+      setLoading(true);
+    setError(null);
     try {
-      const formData = { ...stepData, createdBy: userEmail };
       let response;
-
+ 
       if (activeStep === 0) {
-        response = await axiosInstance.post("/patient/register", formData);
+        response = await axiosInstance.post("/patient/register", {...stepData, createdBy: userEmail});
         if (response.status === 200 || 201 || 202) {
           setPatientId(response.data.patientId);
-          setFormData({ ...formData, patientDetails: formData });
+          setPatientName(
+            response.data.firstName +
+              (response.data.lastName ? " " + response.data.lastName : "")
+          );
+          setFormData({ ...formData, patientDetails: stepData });
           setActiveStep(activeStep + 1);
         }
       } else if (activeStep === 1) {
@@ -81,11 +81,11 @@ const PatientRegistrationPage = () => {
           throw new Error("Patient ID not found. Please complete step 1.");
         }
         response = await axiosInstance.post("/tbdetails/register", {
-          ...formData,
+          ...stepData,
           patientId,
         });
         if (response.status === 200 || 201 || 202) {
-          setFormData({ ...formData, tbDetails: formData });
+          setFormData({ ...formData, tbDetails: stepData });
           setActiveStep(activeStep + 1);
         }
       } else if (activeStep === 2) {
@@ -93,36 +93,53 @@ const PatientRegistrationPage = () => {
           throw new Error("TB Details not found. Please complete step 2.");
         }
         response = await axiosInstance.post("/nikshaymitra/register", {
-          ...formData,
+          ...stepData,
           patientId,
         });
         if (response.status === 200 || 201 || 202) {
-          setFormData({ ...formData, nikshayDetails: formData });
+          setFormData({ ...formData, nikshayDetails: stepData });
           setActiveStep(activeStep + 1);
         }
       } else if (activeStep === 3) {
         if (!formData.nikshayDetails) {
           throw new Error("Nikshay Details not found. Please complete step 3.");
         }
+        console.log(stepData)
         response = await axiosInstance.post("/contactscreening/save", {
-          ...formData,
+          ...stepData,
           patientId,
         });
         if (response.status === 200 || 201 || 202) {
-          setFormData({ ...formData, contactScreeningDetails: formData });
-          navigate(`/patient-dashboard/${patientId}`);
+          setFormData({ ...formData, contactScreeningDetails: stepData });
+          setOpenSnackbar(true);
+          setTimeout(() => {
+              navigate(`/patient-dashboard/${patientId}`);
+          }, 4000);
         }
       }
     } catch (error: any) {
       console.error("Error saving data:", error);
-      alert(`Error: ${error.response?.data?.message || error.message}`);
+      setError(
+        error.response?.data ||
+          "There was an error submitting the form, please try again"
+      );
+    } finally {
+      setLoading(false);
     }
   };
-
+ 
   const handleBack = () => {
     setActiveStep(activeStep - 1);
   };
-
+ 
+  // const handleNext = () => {
+  //   if (activeStep < steps.length - 1) {
+  //     setActiveStep(activeStep + 1);
+  //   } else {
+  //     console.log("Final Data Submitted:", stepData);
+  //   }
+  // };
+ 
   return (
     <>
       <Container maxWidth="md">
@@ -141,7 +158,7 @@ const PatientRegistrationPage = () => {
               </Stepper>
             </Paper>
           </Grid>
-
+ 
           <Grid item xs={8}>
             <Paper sx={{ p: 3 }}>
               {activeStep === 0 && (
@@ -149,7 +166,9 @@ const PatientRegistrationPage = () => {
                   language={language}
                   data={formData.patientDetails}
                   onSave={handleSave}
+                  // onNext={handleNext}
                   functionality="register"
+                  loading={loading}
                 />
               )}
               {activeStep === 1 && (
@@ -157,8 +176,11 @@ const PatientRegistrationPage = () => {
                   language={language}
                   data={formData.tbDetails}
                   onSave={handleSave}
+                  // onNext={handleNext}
                   onBack={handleBack}
                   functionality="register"
+                  patientName={patientName}
+                  loading={loading}
                 />
               )}
               {activeStep === 2 && (
@@ -166,8 +188,11 @@ const PatientRegistrationPage = () => {
                   language={language}
                   data={formData.nikshayDetails}
                   onSave={handleSave}
+                  // onNext={handleNext}
                   onBack={handleBack}
                   functionality="register"
+                  patientName={patientName}
+                  loading={loading}
                 />
               )}
               {activeStep === 3 && (
@@ -178,8 +203,26 @@ const PatientRegistrationPage = () => {
                   onSubmit={handleSubmit(onSubmit)}
                   onBack={handleBack}
                   functionality="register"
+                  patientName={patientName}
+                  loading={loading}
                 />
               )}
+              {error && (
+                <Alert severity="error" sx={{ mt: 3, mb: 2 }}>
+                  {error}
+                </Alert>
+              )}
+              <Snackbar
+                open={openSnackbar}
+                autoHideDuration={4000}
+                onClose={() => setOpenSnackbar(false)}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              >
+                <Alert severity="success" variant="filled">
+                  Patient registered successfully with Personal, TB, Nikshay & Contact
+                  Screening details.
+                </Alert>
+              </Snackbar>
             </Paper>
           </Grid>
         </Grid>
@@ -187,5 +230,5 @@ const PatientRegistrationPage = () => {
     </>
   );
 };
-
+ 
 export default PatientRegistrationPage;
