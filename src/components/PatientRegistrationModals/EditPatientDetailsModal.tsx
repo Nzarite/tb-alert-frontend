9087b76270c555be4e1d8193405bca7bd93001ed
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-03-03 18:18:58 +0530

    Removed posturl contact screening id

--- src/components/PatientRegistrationModals/EditPatientDetailsModal.tsx
+++ src/components/PatientRegistrationModals/EditPatientDetailsModal.tsx
@@ -58,7 +58,7 @@ const EditPatientDetailsModal = ({ open, onClose, prop, patientId }: any) => {
 			case "nikshaymitra":
 				return `/nikshaymitra/register`;
 			case "contactscreening":
-				return `/contactscreening/${patientId}`;
+				return `/contactscreening`;
 			default:
 				return "";
 		}
