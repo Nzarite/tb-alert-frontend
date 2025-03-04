153981c4ee79b8893ad8acd7ea0f62232e8f240f
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2025-03-04 15:32:07 +0530

    minor fix in search box to initialise userroles

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -28,7 +28,7 @@ const SearchBox = ({ changeSearch, role }: SearchProps) => {
   const [error, setError] = useState<string | null>(null);
   const userRoles: Role[] = useSelector(
     (state: any) => state.user?.profile?.client_roles
-  );
+  )||[];
   const userState: string = useSelector((state: any) => state.user?.userState);
 
   const roleToUrlMap: Record<SearchProps["role"], string> = {
