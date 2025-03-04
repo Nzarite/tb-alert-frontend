3b99daae01b9dd8ed3d8d5632a862d93b09ebff0
Tanishq Chaurasia <tanishq.chaurasia@beehyv.com>
2025-03-04 11:25:00 +0530

    addded districts to json file for states

--- src/pages/Registration/Patient/PatientRegistrationPage.tsx
+++ src/pages/Registration/Patient/PatientRegistrationPage.tsx
@@ -27,7 +27,6 @@ import TbDetailsForm, {
   TbDetailsData,
 } from "../../../components/TbDetailsForm/TbDetailsForm";
 import axiosInstance from "../../../components/axiosInstance";
-import { Role } from "../../../components/Authorization/Roles/Types";
 
 const PatientRegistrationPage = () => {
   const location = useLocation();
