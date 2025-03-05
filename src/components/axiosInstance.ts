2a1da46074482003dc6a5bd864697ed9f7b18496
Anubhav <anubhav.bansal@i2oretail.com>
2025-03-05 18:54:37 +0530

    minor changes

--- src/components/axiosInstance.ts
+++ src/components/axiosInstance.ts
@@ -7,7 +7,7 @@ import { useAuth } from "react-oidc-context";
 // import { store } from "../store/store";
 
 const BACKEND_SERVICE_NAME =
-  "http://" + (import.meta.env.VITE_BACKEND_SERVICE_NAME || "localhost");
+  "https://" + (import.meta.env.VITE_BACKEND_SERVICE_NAME || "localhost");
 const SERVER_PORT = import.meta.env.VITE_SPRINGBOOT_HOST_PORT || "8080";
 
 const axiosInstance = axios.create({
