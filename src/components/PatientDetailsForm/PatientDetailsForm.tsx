67c5bbc12e5d4e7bc5665769add220609a635d6a
Akshit Chauhan <akshitsingh.chauhan@beehyv.com>
2025-03-06 21:00:04 +0530

    updates in dashboard

--- src/components/PatientDetailsForm/PatientDetailsForm.tsx
+++ src/components/PatientDetailsForm/PatientDetailsForm.tsx
@@ -34,7 +34,7 @@ export type PatientDetailsData = {
   block: string;
   gp: string;
   consentForMessage: boolean;
-  createdBy:string;
+  createdBy: string;
   reminderTime?: string;
 };
 
@@ -53,69 +53,13 @@ export interface PatientDetailsFormLabelsData {
   consentForMessageLabel: LabelOption;
   currentStatusLabel: string;
   reminderTimeLabel: string;
-  createdByLabel:string;
+  createdByLabel: string;
 }
 
 export type StateOption = {
   label: string;
   value: string;
 };
-
-const [telecallers, setTelecallers] = useState<TeleCaller[]>();
-
-const patientDetailsSchema = z
-  .object({
-    firstName: z.string().min(1, "First Name is required"),
-    lastName: z.string().optional(),
-    gender: z.enum(["Male", "Female"], {
-      errorMap: () => ({ message: "Gender is required" }),
-    }),
-    phoneNumber: z
-      .string()
-      .regex(/^\d+$/, "Contact number must contain only numbers")
-      .min(10, "Contact number must be at least 10 digits")
-      .max(15, "Contact number can't exceed 15 digits"),
-    age: z
-      .number()
-      .int("Age must be an integer")
-      .min(1, "Age must be at least 1")
-      .max(150, "Age must be at most 150"),
-    state: z.string().min(1, "State Name is required"),
-    district: z.string().min(1, "District Name is required"),
-    village: z.string().min(1, "Village Name is required"),
-    block: z.string().min(1, "Block Name is required"),
-    gp: z.string().min(1, "GP Name is required"),
-    consentForMessage: z.boolean({
-      errorMap: () => ({ message: "Consent for message is required" }),
-    }),
-      createdBy: z.string().superRefine((val, ctx) => {
-          if (telecallers && telecallers.length > 0 && !val) {
-              ctx.addIssue({
-                  code: z.ZodIssueCode.custom,
-                  message: "Select a Telecaller",
-              });
-          }
-      }),
-    reminderTime: z.string().optional(),
-  })
-  .superRefine((data, ctx) => {
-    if (data.consentForMessage) {
-      if (!data.reminderTime) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          path: ["reminderTime"],
-          message:
-            "Reminder time is required when consent for message is given",
-        });
-      } else if (!/^\d{2}:\d{2}$/.test(data.reminderTime)) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          path: ["reminderTime"],
-          message: "Reminder time must be in HH:MM format",
-        });
-      }
-    }
-  });
 
 const PatientDetailsForm = ({
   language,
@@ -137,6 +81,7 @@ const PatientDetailsForm = ({
   const [allTelecaller, setAllTelecaller] = useState<
     { value: string; telecallers: string[] }[]
   >([]);
+  const [telecallers, setTelecallers] = useState<TeleCaller[]>();
 
   const auth = useAuth();
 
@@ -164,7 +109,7 @@ const PatientDetailsForm = ({
     consentForMessageLabel: { label: "", options: [] },
     currentStatusLabel: "",
     reminderTimeLabel: "",
-    createdByLabel:"",
+    createdByLabel: "",
   });
 
   useEffect(() => {
@@ -206,7 +151,7 @@ const PatientDetailsForm = ({
 
       setBackendStates(stateNames);
     } catch (error) {
-      if(error.status===401) setBackendStates(userState);
+      if (error.status === 401) setBackendStates(userState);
       console.error("Error fetching states:", error);
     }
   };
@@ -223,6 +168,60 @@ const PatientDetailsForm = ({
       setFilteredStates(filtered);
     }
   }, [backendStates, states]);
+
+  const patientDetailsSchema = z
+    .object({
+      firstName: z.string().min(1, "First Name is required"),
+      lastName: z.string().optional(),
+      gender: z.enum(["Male", "Female"], {
+        errorMap: () => ({ message: "Gender is required" }),
+      }),
+      phoneNumber: z
+        .string()
+        .regex(/^\d+$/, "Contact number must contain only numbers")
+        .min(10, "Contact number must be at least 10 digits")
+        .max(15, "Contact number can't exceed 15 digits"),
+      age: z
+        .number()
+        .int("Age must be an integer")
+        .min(1, "Age must be at least 1")
+        .max(150, "Age must be at most 150"),
+      state: z.string().min(1, "State Name is required"),
+      district: z.string().min(1, "District Name is required"),
+      village: z.string().min(1, "Village Name is required"),
+      block: z.string().min(1, "Block Name is required"),
+      gp: z.string().min(1, "GP Name is required"),
+      consentForMessage: z.boolean({
+        errorMap: () => ({ message: "Consent for message is required" }),
+      }),
+      createdBy: z.string().superRefine((val, ctx) => {
+        if (telecallers && telecallers.length > 0 && !val) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Select a Telecaller",
+          });
+        }
+      }),
+      reminderTime: z.string().optional(),
+    })
+    .superRefine((data, ctx) => {
+      if (data.consentForMessage) {
+        if (!data.reminderTime) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            path: ["reminderTime"],
+            message:
+              "Reminder time is required when consent for message is given",
+          });
+        } else if (!/^\d{2}:\d{2}$/.test(data.reminderTime)) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            path: ["reminderTime"],
+            message: "Reminder time must be in HH:MM format",
+          });
+        }
+      }
+    });
 
   const {
     control,
@@ -248,20 +247,18 @@ const PatientDetailsForm = ({
     }
   }, [selectedState, setValue, allDistricts]);
 
-
-  useEffect(()=>{
-    const fetchTelecaller=async()=>{
+  useEffect(() => {
+    const fetchTelecaller = async () => {
       try {
-        const response=await axiosInstance.get(`/telecaller/state/${selectedState}`);
+        const response = await axiosInstance.get(
+          `/telecaller/state/${selectedState}`
+        );
         setTelecallers(response.data);
         console.log(telecallers);
-      } catch (error) {
-
-      }
-    }
+      } catch (error) {}
+    };
     fetchTelecaller();
-  },[selectedState])
-
+  }, [selectedState]);
 
   useEffect(() => {
     if (data) {
@@ -277,7 +274,7 @@ const PatientDetailsForm = ({
     //   onNext();
     // }
   };
-  console.log(selectedState)
+  console.log(selectedState);
 
   const consentForMessage = useWatch({ control, name: "consentForMessage" });
 
@@ -286,19 +283,34 @@ const PatientDetailsForm = ({
     type: "text" | "select" | "date" | "radio" | "number";
     label: string;
     options?: { label: string; value: string }[];
-    disabled:boolean;
+    disabled: boolean;
   }[] = [
-    { name: "firstName", type: "text", label: labels.firstNameLabel, disabled:false },
-    { name: "lastName", type: "text", label: labels.lastNameLabel, disabled:false },
+    {
+      name: "firstName",
+      type: "text",
+      label: labels.firstNameLabel,
+      disabled: false,
+    },
+    {
+      name: "lastName",
+      type: "text",
+      label: labels.lastNameLabel,
+      disabled: false,
+    },
     {
       name: "gender",
       type: "radio",
       label: labels.genderLabel.label,
       options: labels.genderLabel.options,
-      disabled:false
+      disabled: false,
     },
-    { name: "phoneNumber", type: "text", label: labels.phoneNumberLabel, disabled:false },
-    { name: "age", type: "number", label: labels.ageLabel, disabled:false },
+    {
+      name: "phoneNumber",
+      type: "text",
+      label: labels.phoneNumberLabel,
+      disabled: false,
+    },
+    { name: "age", type: "number", label: labels.ageLabel, disabled: false },
     {
       name: "state",
       type: "select",
@@ -306,7 +318,7 @@ const PatientDetailsForm = ({
       options: userRoles.includes("SuperAdmin")
         ? filteredStates
         : filteredStates.filter((option) => option.value === userState),
-      disabled:false,
+      disabled: false,
     },
     {
       name: "district",
@@ -314,32 +326,40 @@ const PatientDetailsForm = ({
       label: labels.districtLabel,
       options: districts.map((d) => ({ label: d, value: d })),
       disabled: !selectedState,
-
     },
-    { name: "village", type: "text", label: labels.villageLabel, disabled:false },
-    { name: "block", type: "text", label: labels.blockLabel, disabled:false },
-    { name: "gp", type: "text", label: labels.gpLabel, disabled:false },
-      {
-          name: "createdBy",
-          type: "select",
-          label: labels.createdByLabel,
-          options: telecallers?.map((t) => ({ label: t.firstName+" "+t.lastName, value: t.email })),
-          disabled: telecallers?.length==0 || (userRoles.includes("Telecaller") && !userRoles.includes("SuperAdmin") && !userRoles.includes("StateCoordinator")),
-      },
-      {
+    {
+      name: "village",
+      type: "text",
+      label: labels.villageLabel,
+      disabled: false,
+    },
+    { name: "block", type: "text", label: labels.blockLabel, disabled: false },
+    { name: "gp", type: "text", label: labels.gpLabel, disabled: false },
+    {
+      name: "createdBy",
+      type: "select",
+      label: labels.createdByLabel,
+      options: telecallers?.map((t) => ({
+        label: t.firstName + " " + t.lastName,
+        value: t.email,
+      })),
+      disabled:
+        telecallers?.length == 0 ||
+        (userRoles.includes("Telecaller") &&
+          !userRoles.includes("SuperAdmin") &&
+          !userRoles.includes("StateCoordinator")),
+    },
+    {
       name: "consentForMessage",
       type: "radio",
       label: labels.consentForMessageLabel.label,
       options: labels.consentForMessageLabel.options,
-      disabled:false,
+      disabled: false,
     },
-
-
   ];
 
-
   if (!labels || !states) return <CircularProgress />;
-console.log(userRoles)
+  console.log(userRoles);
   return (
     <Box>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -391,7 +411,6 @@ console.log(userRoles)
                   onChange={(e) => controllerField.onChange(e.target.value)}
                   error={!!errors[field.name]}
                   disabled={field.disabled}
-
                   helperText={errors[field.name]?.message}
                 >
                   {field.options?.map((option) => (
